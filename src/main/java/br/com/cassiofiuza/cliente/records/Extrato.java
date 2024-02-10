d4b316a15101b88cc4181092d2d38afb90191d08
Cassio Fiuza <cassio.silva@syonet.com>
2024-02-10 17:24:03 -0300

    chore(Extrato): remove anotação não utilizada

--- src/main/java/br/com/cassiofiuza/cliente/records/Extrato.java
+++ src/main/java/br/com/cassiofiuza/cliente/records/Extrato.java
@@ -2,13 +2,10 @@ package br.com.cassiofiuza.cliente.records;
 
 import java.util.List;
 
-import jakarta.json.bind.annotation.JsonbProperty;
-
 public record Extrato (
   Integer id,
   String nome,
   Integer limite,
   Integer saldo,
-  @JsonbProperty(value = "ultimas_transacoes")
   List<Transacao> ultimasTransacoes
 ){}
