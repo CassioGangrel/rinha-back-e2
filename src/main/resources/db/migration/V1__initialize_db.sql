eb5ccd55df2ab55c5d61712eb1c39dd4fd57c0ca
Cassio Fiuza <cassio.silva@syonet.com>
2024-02-12 02:21:57 -0300

    feat(infra): codigo infra (docker|nginx|postgres)

--- src/main/resources/db/migration/V1__initialize_db.sql
+++ src/main/resources/db/migration/V1__initialize_db.sql
@@ -15,6 +15,8 @@ CREATE TABLE transacoes (
 		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
+CREATE INDEX transacoes_cliente_id_idx ON transacoes (cliente_id);
+
 CREATE TABLE saldos (
 	id SERIAL PRIMARY KEY,
 	cliente_id INTEGER NOT NULL,
@@ -22,6 +24,8 @@ CREATE TABLE saldos (
 	CONSTRAINT fk_clientes_saldos_id
 		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
+
+CREATE INDEX saldos_cliente_id_idx ON saldos (cliente_id);
 
 DO $$
 BEGIN
